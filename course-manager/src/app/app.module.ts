6a7aee08c100a1fb2ba0d32f509e10e68b597271
Wesley AV <13851280+wesleyav@users.noreply.github.com>
2022-08-05 12:00:56 -0300

    Conhecendo a pasta core

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './navbar/nav-bar.component';
 import { Error404Component } from './error404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/component/navbar/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
